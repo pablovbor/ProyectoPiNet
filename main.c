46b28c49082f29a029cc9c7d55832765e5a7ae20
pablovbor <villaborraganp@gmail.com>
2026-04-23 13:14:18 +0200

    comentario de materiales

--- main.c
+++ main.c
@@ -9,6 +9,7 @@ void main(){
 
     printf("Creacion de lista de componentes disponibles");
     printf("Clasificación de componentes");
+    printf("Hacer un comentario con los materiales necesarios");
 
 
     printf("Estoy viendo el tráfico\n", );
